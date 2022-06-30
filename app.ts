c110c58b76cc643168e82305e6694ed2e87f0337
Guilheermeap <guilhermeeap63@gmail.com>
2022-06-30 18:37:57 -0300

    OK

--- app.ts
+++ app.ts
@@ -1,15 +1,68 @@
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1') as HTMLInputElement;
-let input2 = document.getElementById('input2') as HTMLInputElement;
-
-function adicionarNumeros (numero1: number, numero2: number){
-    return numero1 + numero2
+const pessoa ={
+    nome: 'Guilherme',
+    idade: 21,
+    profissao: 'desenvolvedor'
 }
 
-if (button){
-    button.addEventListener('click', ()=> {
-        if (input1 && input2){
-            console.log(adicionarNumeros(Number(input1.value), Number(input2.value)))
-        }
-    })
+pessoa.idade = 40;
+
+const joao: {nome: string, idade: number, profissao: string} = {
+    nome: 'João',
+    idade: 40,
+    profissao: 'pintor'
 }
+
+const ana: {nome: string, idade: number, profissao: string} = {
+    nome: 'Ana',
+    idade: 31,
+    profissao: 'Desenvolvedora'
+}
+
+enum Profissao {
+    Professora,
+    Atriz,
+    Densenvolvedor,
+    JogadorDeFutebol
+}
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao: Profissao
+}
+
+interface Estudante extends Pessoa {
+    materias: string[]
+}
+
+const vanessa: Pessoa = {
+    nome: 'Vanessa',
+    idade: 31,
+    profissao: Profissao.Professora
+}
+
+const maria: Pessoa = {
+    nome: 'Maria',
+    idade: 38,
+    profissao: Profissao.Professora
+}
+
+const jessica: Estudante = {
+    nome: 'Jessia',
+    idade: 20,
+    profissao: Profissao.Atriz,
+    materias: ['Matematica discreta', 'programação']
+}
+
+const monica: Estudante = {
+    nome: 'Monica',
+    idade: 20,
+    profissao: Profissao.Professora,
+    materias: ['Matematica discreta', 'Meio Ambiente']
+}
+
+function listar (lista: string[]) {
+    for (const item of lista) {
+        console.log('- ', item);
+    }
+}
+listar(monica.materias);
